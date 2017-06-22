7e3bf7206f72ede3f213857717272ac98d91cdd2
Luis José Sánchez <luisjose@espiral.io>
2017-06-22 23:30:51 +0200

    Creating components parent and child.

---
+++ src/app/parent/parent.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parent',
+  templateUrl: './parent.component.html',
+  styleUrls: ['./parent.component.css']
+})
+export class ParentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  sayToChild(message) {
+    console.log(message);
+  }
+
+}
